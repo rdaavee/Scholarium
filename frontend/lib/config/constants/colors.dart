c1d74a2975aca670fd2655d2d1b114cf44372af6
rdaavee <rdavee0412@gmail.com>
2024-10-08 01:49:08 +0800

    Added colors for cards

--- frontend/lib/config/constants/colors.dart
+++ frontend/lib/config/constants/colors.dart
@@ -8,11 +8,15 @@ class ColorPalette {
   // Either of these will be in our fonts or background
 
   static const Color accentWhite = Color(0xFFE8E8E8);
+  static const Color textWhite = Colors.white;
   static const Color accentBlack = Color(0xFF222831);
   static const Color bgColor = Color(0xffF1FDF8);
   static const Color errorColor = Color(0xffDC2E2E);
-  static const Color gray = Color(0xffC3C3C3);
+  static const Color gray = Color(0xff6D7278);
   static const Color textAccent = Color(0xFFFFE867);
   static const Color btnColor = Color(0xFF0C7FD0);
   static const Color viewAllColor = Color(0xFF0C7FD0);
+
+  static const Color dutyCardColor = Color(0xFF113462);
+  static const Color accentDutyCardColor = Colors.white;
 }
