6f9ddb41b8c12ff30b92d656aadab1a466564b51
rdaavee <rdavee0412@gmail.com>
2024-10-24 16:45:20 +0800

    Enhanced TextField Validation: Added Email and Password Error Handling, Show/Hide Password Icon, and Validation Requirements

--- frontend/lib/features/widgets/admin_widgets/custom_textfield.dart
+++ frontend/lib/features/widgets/admin_widgets/custom_textfield.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final String labelText;
   final TextEditingController controller;
   final bool isEmail;
   final bool isPhone;
+  final bool allowNumbers;
+  final bool isPassword;
 
   const CustomTextField({
     super.key,
@@ -12,21 +15,105 @@ class CustomTextField extends StatelessWidget {
     required this.controller,
     this.isEmail = false,
     this.isPhone = false,
+    this.allowNumbers = true,
+    this.isPassword = false,
+    s,
   });
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  String? errorMessage;
+  bool _isPasswordVisible = false;
+
+  void validateEmail(String value) {
+    if (value.isEmpty) {
+      setState(() {
+        errorMessage = null;
+      });
+      return;
+    }
+
+    if (widget.isEmail && value.contains('@gmail.com')) {
+      if (!value.endsWith('up@phinmaed.com')) {
+        setState(() {
+          errorMessage = 'Email must end with up@phinmaed.com';
+        });
+      } else {
+        setState(() {
+          errorMessage = null;
+        });
+      }
+    } else {
+      setState(() {
+        errorMessage = null;
+      });
+    }
+  }
+
+  void validatePassword(String value) {
+    if (value.isEmpty) {
+      setState(() {
+        errorMessage = null;
+      });
+      return;
+    }
+
+    if (widget.isPassword) {
+      if (value.length < 8) {
+        setState(() {
+          errorMessage = 'Password must be longer than 8 characters.';
+        });
+      } else if (!value.contains(RegExp(r'[A-Z]'))) {
+        setState(() {
+          errorMessage = 'Uppercase letter is missing.';
+        });
+      } else if (!value.contains(RegExp(r'[a-z]'))) {
+        setState(() {
+          errorMessage = 'Lowercase letter is missing.';
+        });
+      } else if (!value.contains(RegExp(r'[0-9]'))) {
+        setState(() {
+          errorMessage = 'Digit is missing.';
+        });
+      } else if (!value.contains(RegExp(r'[!@#%^&*(),.?":{}|<>]'))) {
+        setState(() {
+          errorMessage = 'Special character is missing.';
+        });
+      } else {
+        setState(() {
+          errorMessage = null;
+        });
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10.0),
       child: TextField(
-        controller: controller,
-        keyboardType: isEmail
+        controller: widget.controller,
+        obscureText: widget.isPassword && !_isPasswordVisible,
+        keyboardType: widget.isEmail
             ? TextInputType.emailAddress
-            : isPhone
+            : widget.isPhone
                 ? TextInputType.phone
                 : TextInputType.text,
+        inputFormatters: !widget.allowNumbers
+            ? [FilteringTextInputFormatter.allow(RegExp(r'[^\d]'))]
+            : null,
+        onChanged: (value) {
+          if (widget.isEmail) {
+            validateEmail(value);
+          } else if (widget.isPassword) {
+            validatePassword(value);
+          }
+        },
         decoration: InputDecoration(
-          labelText: labelText,
+          labelText: widget.labelText,
           labelStyle: const TextStyle(
             color: Colors.grey,
             fontSize: 13,
@@ -42,19 +129,37 @@ class CustomTextField extends StatelessWidget {
               const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8.0),
-            borderSide: BorderSide(color: Colors.grey),
+            borderSide: const BorderSide(color: Colors.grey),
           ),
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(8.0),
-            borderSide: BorderSide(color: Colors.grey),
+            borderSide: const BorderSide(color: Colors.grey),
           ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(12.0),
-            borderSide: BorderSide(
-              color: const Color(0xFF00A4E4),
+            borderSide: const BorderSide(
+              color: Color(0xFF00A4E4),
               width: 2,
             ),
           ),
+          errorText: errorMessage,
+          suffixIcon: widget.isPassword
+              ? IconButton(
+                  icon: Icon(
+                    _isPasswordVisible
+                        ? Icons.visibility
+                        : Icons.visibility_off,
+                    size: 15,
+                  ),
+                  onPressed: () {
+                    setState(
+                      () {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      },
+                    );
+                  },
+                )
+              : null,
         ),
         style: const TextStyle(
           color: Colors.black,
